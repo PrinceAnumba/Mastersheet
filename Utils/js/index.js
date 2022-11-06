ce17dff9b6df9bc52b88188f72ccb763913b000e
Prince Anumba <princeanumba0@gmail.com>
2022-11-06 00:33:50 +0000

    add animation to side menu

--- Utils/js/index.js
+++ Utils/js/index.js
@@ -1,4 +1,38 @@
 
-$(".nai").empty();
-$(".name").empty();
-$(".position").empty();
+// $(".nai").empty();
+// $(".name").empty();
+// $(".position").empty();
+
+
+
+
+$("nav").hover(function() {
+  $(".profile_image").attr("src","./Utils/images/MASTERSHEET-White 1.png");
+  $("nav .wrap .navbar .top-nav .logo").css("margin-right","0px");
+  $("nav .wrap .navbar .user").css("width","100%");
+  $("nav .wrap .navbar .user img").css("width","50px");
+  $("nav .wrap .navbar .user").css("align-items","center");
+  $("nav .navbar .top-nav img").css("height","25px");
+  $("nav .navbar .user img").addClass("text-center");
+  $("nav .navbar .menu-list a p").removeClass("nai")
+  $("nav .wrap .navbar .user .name").removeClass("nai");
+  $("nav .wrap .navbar .user .position").removeClass("nai");
+  $("nav .logout a p").removeClass("nai");
+});
+
+
+$("nav").mouseleave(function() {
+  $(".profile_image").attr("src","./Utils/images/MASTERSHEET ROUND 1.png");
+  $("nav .wrap .navbar .top-nav .logo").css("margin-right","130px");
+  $("nav .navbar .top-nav img").css("height","50px");
+  $("nav .navbar .user img").css("height","50px");
+  $("nav .navbar .menu-list a p").addClass("nai");
+  $("nav .logout a p").addClass("nai");
+  $("nav .wrap .navbar .user .name").addClass("nai");
+  $("nav .wrap .navbar .user .position").addClass("nai");
+});
+
+
+// $(this).css("background-color","red")
+
+ 
