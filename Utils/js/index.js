0804881a42a0a1cea2e48a3fc66541b26680fe43
Prince Anumba <princranumba0@gmail.com>
2023-01-10 12:54:28 +0000

    library done

--- Utils/js/index.js
+++ Utils/js/index.js
@@ -1,7 +1,26 @@
 
+<<<<<<< HEAD
 // $(".nai").empty();
 // $(".name").empty();
 // $(".position").empty();
+=======
+// Dragging to upload an image
+"use strict";
+function dragNdrop(event) {
+    var fileName = URL.createObjectURL(event.target.files[0]);
+    var preview = document.getElementById("preview");
+    var previewImg = document.createElement("img");
+    previewImg.setAttribute("src", fileName);
+    preview.innerHTML = "";
+    preview.appendChild(previewImg);
+}
+function drag() {
+    document.getElementById('uploadFile').parentNode.className = 'draging dragBox';
+}
+function drop() {
+    document.getElementById('uploadFile').parentNode.className = 'dragBox';
+}
+>>>>>>> b42df85 (library done)
 
 
 
